6e2bccad791046a12a2dc3af92498e68139fbb71
Aeron <dequitoaeron353@gmail.com>
2025-03-29 00:08:48 +0800

    pseudocode

--- center_alternative.py
+++ center_alternative.py
@@ -1,3 +1,3 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
-# Create a function to replicate lower()
+# Create a function to replicate center()
 # Implement
