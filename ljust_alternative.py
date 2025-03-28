ada6700c4ecbaf021f3469152ffb3f811c829f90
Aeron <dequitoaeron353@gmail.com>
2025-03-29 00:06:58 +0800

    pseudocode

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,3 +1,4 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
-# Create a function to replicate lower()
-# Implement
+# Create a function to replicate ljust()
+# Implement it
+
