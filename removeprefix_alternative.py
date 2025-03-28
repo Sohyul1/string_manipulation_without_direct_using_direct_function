077df699eff770bb22a68d90b140a46608d56519
Aeron <dequitoaeron353@gmail.com>
2025-03-28 23:56:35 +0800

    pseudocode

---
+++ removeprefix_alternative.py
@@ -0,0 +1,3 @@
+# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+#  Create a function to replicate removeprefix()
+# Implement
