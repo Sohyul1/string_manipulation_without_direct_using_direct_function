d39385723611a4dd3af753bd51a6ca7f592978c7
Aeron <dequitoaeron353@gmail.com>
2025-03-28 23:54:45 +0800

    pseudocode

---
+++ no_use_of_lstrip.py
@@ -0,0 +1,3 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+# Create a function to replicate lstrip()
+# Implement
