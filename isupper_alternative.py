298abc159d2a2217424179c33aeca9877d74230c
Aeron <dequitoaeron353@gmail.com>
2025-03-28 23:59:25 +0800

    pseudocode

---
+++ isupper_alternative.py
@@ -0,0 +1,3 @@
+# Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+# Create a function to replicate isupper()
+# Implement
