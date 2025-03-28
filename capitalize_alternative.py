c8694f6ca356601652a2fe7d34f14237f1500ead
Aeron <dequitoaeron353@gmail.com>
2025-03-29 00:13:53 +0800

    pseudocode

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -1,3 +1,3 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
-# Create a function to replicate ljust()
+# Create a function to replicate capitalize()
 # Implement
