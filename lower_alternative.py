9d397fcfd99673d3ab1f4d939d80900371517973
Aeron <dequitoaeron353@gmail.com>
2025-03-28 23:57:53 +0800

    pseudocode

---
+++ lower_alternative.py
@@ -0,0 +1,3 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+# Create a function to replicate lower()
+# Implement
