d130e95b58e293a803765f9bcd415f54392ae881
Aeron <dequitoaeron353@gmail.com>
2025-03-29 00:10:24 +0800

    pseudocode

---
+++ swapcase_alternative.py
@@ -0,0 +1,3 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+# Create a function to replicate swapcase()
+# Implement
