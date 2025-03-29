e426c8e6f348be07cfb06d2c52021ea3e7c4445d
Aeron <dequitoaeron353@gmail.com>
2025-03-29 17:29:08 +0800

    made a variable for sam str, splitted words and new words

--- title_alternative.py
+++ title_alternative.py
@@ -1,3 +1,11 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# Sample String
+sample_string = " hi IM unDER tHe WaTER"
+
 # Create a function to replicate title()
+words_split = sample_string.split()
+new_words = ""
+
+
 # Implement
