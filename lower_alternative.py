3fa99d662f3c9eb59a39cda93a3c0552e8a275fa
Aeron <dequitoaeron353@gmail.com>
2025-03-29 15:18:31 +0800

    made a smple string and alpahbet list

--- lower_alternative.py
+++ lower_alternative.py
@@ -1,3 +1,12 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# Sample string 
+smpl_string = "Hi HelLo hoW ArE YOU"
+
+# List of alphabets 
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+
 # Create a function to replicate lower()
+
 # Implement
