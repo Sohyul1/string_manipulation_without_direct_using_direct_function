b9aab9b67c87e772958de707ce510e58e42ca5db
Aeron <dequitoaeron353@gmail.com>
2025-03-29 16:40:53 +0800

    made a variables for samp string, total char, adn spaces to add

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,4 +1,14 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# Sample string 
+sample_string = "hello"
+total_characters = 15
+
+# Sapces to add 
+spaces = total_characters - len(sample_string)
+
 # Create a function to replicate ljust()
+
+
 # Implement it
 
