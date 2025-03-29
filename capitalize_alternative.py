30a54aacb2ceb1afcc74700ba755bf477ad51be9
Aeron <dequitoaeron353@gmail.com>
2025-03-29 17:21:48 +0800

    used indexing and upper to capitalize only the first letter then add the remaining str in lowercase using lower

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -1,3 +1,10 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# Sample string
+sample_string = "hOW many WORDS are HERE?"
+
 # Create a function to replicate capitalize()
+if len(sample_string) > 0:
+    new_string = sample_string[0].upper() + sample_string[1:].lower()
+
 # Implement
