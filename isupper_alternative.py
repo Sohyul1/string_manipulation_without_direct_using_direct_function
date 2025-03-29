d4ea7cc10fd60168c250e394d8532d5d7275a3c5
Aeron <dequitoaeron353@gmail.com>
2025-03-29 16:18:28 +0800

    use .alpha and .upper to check if the cahracters is alphabet and uppercase

--- isupper_alternative.py
+++ isupper_alternative.py
@@ -1,3 +1,12 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# Sample string
+sample_string = "HELLO 123"
+
 # Create a function to replicate isupper()
+for char in sample_string:
+    if char.isalpha() and char != char.upper(): 
+        
+
+
 # Implement
