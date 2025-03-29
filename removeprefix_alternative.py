7853d861ba626003f752a305070353e894bea3c1
Aeron <dequitoaeron353@gmail.com>
2025-03-29 15:12:13 +0800

    used the same idea from prog 1 which is to find the index of the prefix and then start from the next character when printing, thats why there is a plus len(prefix)

--- removeprefix_alternative.py
+++ removeprefix_alternative.py
@@ -1,3 +1,13 @@
 # Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+# Sample string
+smpl_string = "AeroGel"
+prefix = "Aero"
+
 #  Create a function to replicate removeprefix()
+if smpl_string.startswith(prefix):
+    index = smpl_string.index(prefix) + len(prefix)
+    new_string = smpl_string[index:]
+
 # Implement
+print(new_string)
