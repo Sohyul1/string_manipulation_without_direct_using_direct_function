91335ec9facd2b05ed295e754fd1d2d4fe233efe
Aeron <dequitoaeron353@gmail.com>
2025-03-29 16:51:09 +0800

    used if statements to determine if the spaces needed is greater that 0, and then multipy it to white spaces + the sample string

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,14 +1,17 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # Sample string 
-sample_string = "hello"
-total_characters = 15
+sample_string = "hello HAHAHAHAHAHa"
+total_characters = 50
 
 # Sapces to add 
 spaces = total_characters - len(sample_string)
 
 # Create a function to replicate ljust()
-
+if spaces > 0:
+    new_string = " " * abs(spaces) + sample_string
+else:
+    new_string = sample_string
 
 # Implement it
-
+print(new_string)
