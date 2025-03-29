77d4f609b966845fff1ddb0fb5d8dcf05cf3f183
Aeron <dequitoaeron353@gmail.com>
2025-03-29 17:46:31 +0800

    made a varibale for samp str and initialize for new srting

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,3 +1,10 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# Sample String
+sample_string = "HeLlO, i'M uNdEr ThE wAtEr"
+new_string = ""
+
 # Create a function to replicate swapcase()
+
+
 # Implement
