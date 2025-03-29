c124b9f5bc302058cbbc154396a02ace68317b67
Aeron <dequitoaeron353@gmail.com>
2025-03-29 14:51:33 +0800

    used index function instead to find the position of the first letter and then print the string starting from the first letter

--- no_use_of_lstrip.py
+++ no_use_of_lstrip.py
@@ -1,12 +1,14 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # Sample string
-sam_string = "   Dek i"
+sample_string = "                       Deki iked"
 
 # Create a function to replicate lstrip()
-for i in sam_string:
-    if i == " ":
-        new_string = sam_string.replace(" ", "")
+for i in sample_string:
+    if i != " ":
+        letter_position = sample_string.index(i)
+        break
 
-# Implement
+ # Implement 
+new_string = sample_string[letter_position:]
 print(new_string)
