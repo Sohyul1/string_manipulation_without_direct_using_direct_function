fa276a9ae61921740f1b5008b81790730428bd03
Aeron <dequitoaeron353@gmail.com>
2025-03-29 14:32:35 +0800

    made a for loop to  test every character in samople string, used replace to remove the spaces, working but still not perfect

--- no_use_of_lstrip.py
+++ no_use_of_lstrip.py
@@ -1,3 +1,12 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# Sample string
+sam_string = "   Dek i"
+
 # Create a function to replicate lstrip()
+for i in sam_string:
+    if i == " ":
+        new_string = sam_string.replace(" ", "")
+
 # Implement
+print(new_string)
