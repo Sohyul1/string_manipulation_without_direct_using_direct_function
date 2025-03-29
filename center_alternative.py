63eec001d43fe9a1e3d009c397318c7525432e3b
Aeron <dequitoaeron353@gmail.com>
2025-03-29 17:07:57 +0800

    made a calculations for spaces each side and then used that as the multiplier instead

--- center_alternative.py
+++ center_alternative.py
@@ -1,15 +1,17 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # Sample string
-sample_string = "hello"
-total_characters = 15
+sample_string = "hello I am under the water"
+total_characters = 30
 
 # Spaces to add 
 spaces =  total_characters - len(sample_string)
+left_space = spaces // 2
+right_space = spaces - left_space
 
 # Create a function to replicate center()
 if spaces > 0:
-    new_string = " " * abs(spaces) + sample_string + " " * abs(spaces) 
+    new_string = " " * left_space + sample_string + " " * right_space
 else:
     new_string = sample_string
 
