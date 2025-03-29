80f2e8ec245c5fd6ba78f84557cfd37ac0ad1e8b
Aeron <dequitoaeron353@gmail.com>
2025-03-29 17:55:40 +0800

    Add another condition so that the numbers wont get deleted

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,15 +1,17 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # Sample String
-sample_string = "HeLlO, i'M uNdEr ThE wAtEr"
+sample_string = "HeLlO, i'M uNdEr ThE wAtEr 123"
 new_string = ""
 
 # Create a function to replicate swapcase()
 for char in sample_string:
     if char.islower():
         new_string += char.upper()
-    else:
+    elif char.isupper():
         new_string += char.lower()
+    else:
+        new_string += char
 
 # Implement
 print(new_string)
