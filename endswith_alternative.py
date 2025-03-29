23631887f854173ace375bdc40c811eb082b2cff
Aeron <dequitoaeron353@gmail.com>
2025-03-29 16:37:41 +0800

    used len as the -index of the suffix and then used if statements to test if it ends with the givern suffix

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,3 +1,13 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# Sample string and suffix
+sample_string = "hello_world"
+suffix = "world"
+
 # Create a function to replicate endswith()
-# Implement
+index = len(suffix)
+if sample_string[-index:] == suffix:
+    print("YES") 
+else:
+    print("NO")  
+
