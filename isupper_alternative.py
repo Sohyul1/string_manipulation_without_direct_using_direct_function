52de9a05ca12a926ba0d741d7647c68b466ffdb7
Aeron <dequitoaeron353@gmail.com>
2025-03-29 16:27:04 +0800

    used def to check for other smaple strings

--- isupper_alternative.py
+++ isupper_alternative.py
@@ -1,12 +1,16 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # Sample string
-sample_string = "HELLO 123"
-
+sample_string1 = "HELLO"
+sample_string2 = "hello hi"
 # Create a function to replicate isupper()
-for char in sample_string:
-    if char.isalpha() and char != char.upper(): 
-        
+def uppercase_checker(s):
+    for char in s:
+        if char.isalpha() and char != char.upper():        
+        # Implement
+            print("NO")
+            return
+    print("YES")
 
-
-# Implement
+uppercase_checker(sample_string1)
+uppercase_checker(sample_string2)
