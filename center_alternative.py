a58af4eb48618d63ebab4373b0f9d22d7e52a301
Aeron <dequitoaeron353@gmail.com>
2025-03-29 16:58:52 +0800

    made variables for sample str, tottal char, adnb spces to add

--- center_alternative.py
+++ center_alternative.py
@@ -1,3 +1,12 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Sample string
+sample_string = "hello"
+total_characters = 15
+# Spaces to add 
+spaces =  total_characters - len(sample_string)
+
 # Create a function to replicate center()
+
+
 # Implement
