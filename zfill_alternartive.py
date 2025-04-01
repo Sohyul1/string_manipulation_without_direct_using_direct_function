5a5fb8205104a3e3b4d0a2f3504ac4f31b9c4da6
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:38:32 +0800

    pseudocode

---
+++ zfill_alternartive.py
@@ -0,0 +1,5 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+# Sample string
+# Zeros to add 
+# Create a function that replicates zfill()
+# Implement 
