995c7ce0aa0e5368a4829b1d4919a735b96d7c09
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:31:35 +0800

    made the varibales needed

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,5 +1,13 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
 # Sample String
+sample_string = "hello HAHAHAHAHAH"
+total_characters = 50
+
 # Spaces to add 
+spaces = total_characters - len(sample_string)
+
 # Create a function the replicates rjust()
+
+
 # Implement
