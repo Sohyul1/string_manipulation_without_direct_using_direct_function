a9d89957373289c6adaeb061923ca85e71150c97
Aeron <dequitoaeron353@gmail.com>
2025-04-01 20:06:23 +0800

    same logic with count, just removed the count

--- index_alternative.py
+++ index_alternative.py
@@ -1,4 +1,13 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
 # Sample string
+sample_string = "hello world"
+substring = "world"
+
 # Create a funtion that replicates index()
-# Implement 
+for i  in range(len(sample_string)):
+    if sample_string[i:i + len(substring)] == substring:
+        # Implement
+        print(i)
+        break
+ 
