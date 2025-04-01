9dd5dfb3a1d5478990493b6ff64c3e96d825bce9
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:00:11 +0800

    made every varibale needed

--- upper_alternative.py
+++ upper_alternative.py
@@ -1,6 +1,15 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
 # Sample String
+smpl_string = "Hi HelLo hoW ArE YOU"
+
 # List of alphabet 
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+
 # New string
+new_string = ""
+
+
 # Create a function that replicates .upper()
 # Implement
