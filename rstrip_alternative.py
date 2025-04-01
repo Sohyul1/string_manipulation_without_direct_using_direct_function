6708a30a15bb01183670670de81a9ccc6703bf5d
Aeron <dequitoaeron353@gmail.com>
2025-04-01 18:38:59 +0800

    copied my lstrip but changes some things, used reveresed in loop so that it will take characters from right to left, same with index, i used rindex instead, then transfered the letter position to the left  + 1 so it will get the nlast letter

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,4 +1,14 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
 # Sample string
+sample_string = "Deki iked      "
+
 # Create a function to replicate rstrip()
+for i in reversed(sample_string):
+    if i != " ":
+        letter_position = sample_string.rindex(i)
+        break
+
 # Implement 
+new_string = sample_string[:letter_position + 1]
+print(new_string)
