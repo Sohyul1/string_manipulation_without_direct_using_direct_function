73638d4e5aba410cfdf3cf86d29f6c5282738f20
Aeron <dequitoaeron353@gmail.com>
2025-04-01 18:27:22 +0800

    pseudocode

---
+++ rstrip_alternative.py
@@ -0,0 +1,4 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+# Sample string
+# Create a function to replicate rstrip()
+# Implement 
