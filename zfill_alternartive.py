28056191a920fc0a9f079b28a39d08df92c3acaa
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:48:05 +0800

    copied my ljust but used 0 instead of sapces, also converted the int to str

--- zfill_alternartive.py
+++ zfill_alternartive.py
@@ -1,5 +1,17 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
 # Sample string
+sample_number = 123
+total_characters = 5
+
 # Zeros to add 
+zeros_to_add = total_characters - len(str(sample_number))
+
 # Create a function that replicates zfill()
+if zeros_to_add > 0:
+    new_num = "0" * zeros_to_add + str(sample_number)
+else:
+    new_num = sample_number
+
 # Implement 
+print(new_num)
