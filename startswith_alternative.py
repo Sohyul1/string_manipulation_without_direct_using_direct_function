1336e6bf5909f339772069d7f1a3b8c648eb1020
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:18:05 +0800

    pseudocode

---
+++ startswith_alternative.py
@@ -0,0 +1,4 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+# Sample string and starting character
+# Create a funtionthat replicates startswith()
+# Implement
