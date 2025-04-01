4da6330c5e662381ce46239fe291581308db9acb
Aeron <dequitoaeron353@gmail.com>
2025-04-01 18:56:06 +0800

    almost same wiht the removeprefix, i just used endswith instead and then difference of len of both and tranfered the index to the stop position

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,4 +1,16 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
 # Sample string 
+smpl_string = "AeroGel"
+suffix = "Gel"
+
 # Create a function that replicates remove suffix 
+if smpl_string.endswith(suffix):
+    index = len(smpl_string) - len(suffix)
+    new_string = smpl_string[:index]
+
+else:
+    new_string = smpl_string
+
 # Implement
+print(new_string)
