9be90ea92558c19b4eca971c9a19f6ed4593e9d7
Aeron <dequitoaeron353@gmail.com>
2025-04-01 20:21:11 +0800

    sopied my index alternative code, i just changed the stop on the loop and the skip to make it start from the right

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -1,4 +1,13 @@
-# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
 # Sample string
+sample_string = "hello world and hello to you"
+substring = "hello"
+
 # Create a funtion that replicates index()
-# Implement 
+for i  in range(len(sample_string) - len(substring), -1, -1):
+    if sample_string[i:i + len(substring)] == substring:
+        # Implement
+        print(i)
+        break
+
