8c6a731cc8e6ad76718d90dbfd0cd26d31d1da56
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:13:39 +0800

    did the same as isupper, i just changed the char.upper to char.lower

--- islower_alternative.py
+++ islower_alternative.py
@@ -1,4 +1,17 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
 # Sample String
+sample_string1 = "HELLO"
+sample_string2 = "hello hi"
+
 # Create a function that replicates islower()
-# Implement
+def uppercase_checker(s):
+    for char in s:
+        if char.isalpha() and char != char.lower(): 
+        # Implement
+            print("No")
+            return
+    print("Yes")
+
+uppercase_checker(sample_string1)
+uppercase_checker(sample_string2)
