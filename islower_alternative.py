4392d220244d173ded4c7529da23a9a84079a8e2
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:08:41 +0800

    pseudocode

---
+++ islower_alternative.py
@@ -0,0 +1,4 @@
+# Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+# Sample String
+# Create a function that replicates islower()
+# Implement
