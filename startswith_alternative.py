69db28008f589b4578d9529a7116c99ea3db3dd4
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:27:42 +0800

    copied my endswith and reversed it

--- startswith_alternative.py
+++ startswith_alternative.py
@@ -1,4 +1,13 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
 # Sample string and starting character
+sample_string = "hello_world"
+startswith = "hello"
+
 # Create a funtionthat replicates startswith()
-# Implement
+index = len(startswith)
+if sample_string[:index] == startswith:
+    print("Yes")
+else:
+    print("NO")
+
