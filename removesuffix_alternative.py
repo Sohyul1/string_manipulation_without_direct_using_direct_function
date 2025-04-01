254a1377ad4477b9ad6c6eace724db433eda34a5
Aeron <dequitoaeron353@gmail.com>
2025-04-01 18:41:28 +0800

    pseudocode

---
+++ removesuffix_alternative.py
@@ -0,0 +1,4 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+# Sample string 
+# Create a function that replicates remove suffix 
+# Implement
