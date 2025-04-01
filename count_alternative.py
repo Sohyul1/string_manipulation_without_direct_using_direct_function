616c6bd4ebef81f7a50b7a5bfca8d15192014443
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:55:17 +0800

    made the varibales needed

--- count_alternative.py
+++ count_alternative.py
@@ -1,4 +1,13 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# Sample string
+sample_string = "hello world, hello there"
+substring = "hello"
+
 # Initialize count 
+count = 0
+
 # Create a function that repliicates count()
+
+
 # Implement
