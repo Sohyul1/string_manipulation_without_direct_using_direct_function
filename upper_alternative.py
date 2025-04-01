c003c00305075dc2a6bc73ab3aaaffbb2906ac0e
Aeron <dequitoaeron353@gmail.com>
2025-04-01 18:58:29 +0800

    pseudocode

---
+++ upper_alternative.py
@@ -0,0 +1,6 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+# Sample String
+# List of alphabet 
+# New string
+# Create a function that replicates .upper()
+# Implement
