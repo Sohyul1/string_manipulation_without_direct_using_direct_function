66975ef18031195648cccf57d46ad3897a1256d0
Aeron <dequitoaeron353@gmail.com>
2025-04-01 19:53:45 +0800

    pseudocode

---
+++ count_alternative.py
@@ -0,0 +1,4 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+# Initialize count 
+# Create a function that repliicates count()
+# Implement
