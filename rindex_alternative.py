67c89f9d92a9c1f8963a3cf7893859592c6bae2b
Aeron <dequitoaeron353@gmail.com>
2025-04-01 20:08:06 +0800

    pseudocode

---
+++ rindex_alternative.py
@@ -0,0 +1,4 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+# Sample string
+# Create a funtion that replicates index()
+# Implement 
